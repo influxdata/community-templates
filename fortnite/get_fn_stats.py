e63af268f6931452b91ac8388ae22ecfd9dfd649
adam <tengen+github@gmail.com>
2020-08-29 10:49:22 -0700

    remove python 3 requirement and add more validations

--- fortnite/get_fn_stats.py
+++ fortnite/get_fn_stats.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import io
 import time
 import json
 import csv
@@ -14,7 +15,7 @@ else:
     sys.exit(1)
 
 url = 'https://fortniteapi.io/stats'
-season = 13
+season = 14
 
 data_api = {}
 
@@ -24,50 +25,62 @@ def parse_data(data):
     data_api['account_level'] = data['account']['level']
     data_api['account_progress_pct'] = data['account']['progress_pct']
 
-    data_api['solo_placetop1'] = data['global_stats']['solo']['placetop1']
-    data_api['solo_placetop3'] = data['global_stats']['solo']['placetop3']
-    data_api['solo_placetop5'] = data['global_stats']['solo']['placetop5']
-    data_api['solo_placetop10'] = data['global_stats']['solo']['placetop10']
-    data_api['solo_placetop25'] = data['global_stats']['solo']['placetop25']
-    data_api['solo_kd'] = data['global_stats']['solo']['kd']
-    data_api['solo_winrate'] = data['global_stats']['solo']['winrate']
-    data_api['solo_kills'] = data['global_stats']['solo']['kills']
-    data_api['solo_matchesplayed'] = data['global_stats']['solo']['matchesplayed']
-    data_api['solo_minutesplayed'] = data['global_stats']['solo']['minutesplayed']
-    data_api['solo_score'] = data['global_stats']['solo']['score']
-    data_api['solo_playersoutlived'] = data['global_stats']['solo']['playersoutlived']
-
-    data_api['duo_placetop1'] = data['global_stats']['duo']['placetop1']
-    data_api['duo_placetop3'] = data['global_stats']['duo']['placetop3']
-    data_api['duo_placetop5'] = data['global_stats']['duo']['placetop5']
-    data_api['duo_placetop10'] = data['global_stats']['duo']['placetop10']
-    data_api['duo_placetop25'] = data['global_stats']['duo']['placetop25']
-    data_api['duo_kd'] = data['global_stats']['duo']['kd']
-    data_api['duo_winrate'] = data['global_stats']['duo']['winrate']
-    data_api['duo_kills'] = data['global_stats']['duo']['kills']
-    data_api['duo_matchesplayed'] = data['global_stats']['duo']['matchesplayed']
-    data_api['duo_minutesplayed'] = data['global_stats']['duo']['minutesplayed']
-    data_api['duo_score'] = data['global_stats']['duo']['score']
-    data_api['duo_playersoutlived'] = data['global_stats']['duo']['playersoutlived']
-
-    data_api['squad_placetop1'] = data['global_stats']['squad']['placetop1']
-    data_api['squad_placetop3'] = data['global_stats']['squad']['placetop3']
-    data_api['squad_placetop5'] = data['global_stats']['squad']['placetop5']
-    data_api['squad_placetop10'] = data['global_stats']['squad']['placetop10']
-    data_api['squad_placetop25'] = data['global_stats']['squad']['placetop25']
-    data_api['squad_kd'] = data['global_stats']['squad']['kd']
-    data_api['squad_winrate'] = data['global_stats']['squad']['winrate']
-    data_api['squad_kills'] = data['global_stats']['squad']['kills']
-    data_api['squad_matchesplayed'] = data['global_stats']['squad']['matchesplayed']
-    data_api['squad_minutesplayed'] = data['global_stats']['squad']['minutesplayed']
-    data_api['squad_score'] = data['global_stats']['squad']['score']
-    data_api['squad_playersoutlived'] = data['global_stats']['squad']['playersoutlived']
+    try:
+        if data['global_stats']['solo']['placetop1'] >= 0:
+            data_api['solo_placetop1'] = data['global_stats']['solo']['placetop1']
+            data_api['solo_placetop3'] = data['global_stats']['solo']['placetop3']
+            data_api['solo_placetop5'] = data['global_stats']['solo']['placetop5']
+            data_api['solo_placetop10'] = data['global_stats']['solo']['placetop10']
+            data_api['solo_placetop25'] = data['global_stats']['solo']['placetop25']
+            data_api['solo_kd'] = data['global_stats']['solo']['kd']
+            data_api['solo_winrate'] = data['global_stats']['solo']['winrate']
+            data_api['solo_kills'] = data['global_stats']['solo']['kills']
+            data_api['solo_matchesplayed'] = data['global_stats']['solo']['matchesplayed']
+            data_api['solo_minutesplayed'] = data['global_stats']['solo']['minutesplayed']
+            data_api['solo_score'] = data['global_stats']['solo']['score']
+            data_api['solo_playersoutlived'] = data['global_stats']['solo']['playersoutlived']
+    except KeyError:
+        pass
     
+    try:
+        if data['global_stats']['duo']['placetop1'] >= 0:
+            data_api['duo_placetop1'] = data['global_stats']['duo']['placetop1']
+            data_api['duo_placetop3'] = data['global_stats']['duo']['placetop3']
+            data_api['duo_placetop5'] = data['global_stats']['duo']['placetop5']
+            data_api['duo_placetop10'] = data['global_stats']['duo']['placetop10']
+            data_api['duo_placetop25'] = data['global_stats']['duo']['placetop25']
+            data_api['duo_kd'] = data['global_stats']['duo']['kd']
+            data_api['duo_winrate'] = data['global_stats']['duo']['winrate']
+            data_api['duo_kills'] = data['global_stats']['duo']['kills']
+            data_api['duo_matchesplayed'] = data['global_stats']['duo']['matchesplayed']
+            data_api['duo_minutesplayed'] = data['global_stats']['duo']['minutesplayed']
+            data_api['duo_score'] = data['global_stats']['duo']['score']
+            data_api['duo_playersoutlived'] = data['global_stats']['duo']['playersoutlived']
+    except KeyError:
+        pass
+    
+    try:
+        if data['global_stats']['squad']['placetop1'] >= 0:
+            data_api['squad_placetop1'] = data['global_stats']['squad']['placetop1']
+            data_api['squad_placetop3'] = data['global_stats']['squad']['placetop3']
+            data_api['squad_placetop5'] = data['global_stats']['squad']['placetop5']
+            data_api['squad_placetop10'] = data['global_stats']['squad']['placetop10']
+            data_api['squad_placetop25'] = data['global_stats']['squad']['placetop25']
+            data_api['squad_kd'] = data['global_stats']['squad']['kd']
+            data_api['squad_winrate'] = data['global_stats']['squad']['winrate']
+            data_api['squad_kills'] = data['global_stats']['squad']['kills']
+            data_api['squad_matchesplayed'] = data['global_stats']['squad']['matchesplayed']
+            data_api['squad_minutesplayed'] = data['global_stats']['squad']['minutesplayed']
+            data_api['squad_score'] = data['global_stats']['squad']['score']
+            data_api['squad_playersoutlived'] = data['global_stats']['squad']['playersoutlived']
+    except KeyError:
+        pass
+
     return data_api
 
 data_api_list = []
 
-with open('/etc/telegraf/players.csv', newline='') as csvfile:
+with io.open('/etc/telegraf/players.csv', newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
         acct_id = row['acct_id']
@@ -84,7 +97,7 @@ with open('/etc/telegraf/players.csv', newline='') as csvfile:
         data = response.json()
 
         # if account level is set to 'None', "Show on Career Leaderboard" may be set to Off
-        if data['account']['level']:
+        if data['account']['level'] and data['global_stats']:
             data_api_dict = parse_data(data)
             data_api_dict['acct_id'] = acct_id
             data_api_dict['pro'] = pro
