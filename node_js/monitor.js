13f20ae2f078a214091565382def5ecb055852fd
mirek.malecha@bonitoo.io <mirek.malecha@bonitoo.io>
2021-02-05 09:41:21 +0100

    Update Node.js monitoring template

--- node_js/monitor.js
+++ node_js/monitor.js
@@ -29,6 +29,7 @@ function writeProcessUsage() {
   // https://nodejs.org/api/process.html#process_process_cpuusage_previousvalue
   writeAPI.writePoint(createPoint('node_cpu_usage', process.cpuUsage()))
   // https://nodejs.org/api/process.html#process_process_resourceusage
+
   // available since node v12.6
   if (process.resourceUsage) {
     writeAPI.writePoint(
